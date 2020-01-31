3ae8b4b1d991c15ff09b1326a64f56cf4759789c
ofeargul <Chadfarrell@live.com>
2020-01-31 04:01:35 +0000

    1.2.1

---
+++ main.go
@@ -0,0 +1,13 @@
+//Programer name: Chad Farrell
+//Date completed: 1.30.2020
+// Description: All about me
+
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("my name is chad, and my major is bussiness")
+  fmt.Println("This is my 3rd semester at butte college")
+  fmt.Println("One of my favorite pastimes is videogames")
+}
