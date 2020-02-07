2004d899c46759abdf390b8ae73287db2b5617de
ofeargul <Chadfarrell@live.com>
2020-02-07 04:21:09 +0000

    Lab 1.4.1

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
-//Programer name: Chad Farrell
-//Date completed: 1.30.2020
-// Description: All about me
+// Programmer name: Chad Farrell
+// Date completed: 2.6.2020
+// Description: names and ages
 
 package main
 
-import "fmt"
+import "fmt" 
 
 func main() {
-  fmt.Println("my name is chad, and my major is bussiness")
-  fmt.Println("This is my 3rd semester at butte college")
-  fmt.Println("One of my favorite pastimes is videogames")
+    //declare variable for favorite food and store your favorite food.
+    var Food = "pizza"
+
+
+    //declare variables for name and age (make sure they are appropriate data types)
+    var age int
+    var name string 
+
+    //ask the user to enter their answer for name and age.
+    fmt.Println("please enter your name")
+    fmt.Scanln(&name)
+    fmt.Println("Please enter your age") 
+    fmt.Scanln(&age)
+    //output a welcome statement using their name
+    fmt.Println("hi", name )
+    //output a statement that says At their age you enjoyed the favorite food
+    fmt.Println("when I was", age, "i liked", Food, "alot")
 }
+ 
