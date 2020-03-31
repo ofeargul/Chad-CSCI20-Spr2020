4614cee6a02370f8137a498787873776b5c60235
ofeargul <Chadfarrell@live.com>
2020-03-31 04:13:06 +0000

    3.1

--- main.go
+++ main.go
@@ -1,23 +1,24 @@
 // Programmer name: Chad Farrell
 // Date completed: 3.26.2020
-// Description: this function creates art
+// Description: this function creates plays britney spears
 
 package main
 
 import "fmt"
 
 //create a function that draws ASCII art of some type
-func art(){
-  fmt.Println("H      H            IIIIIIII")
-  fmt.Println("H      H                I   ")
-  fmt.Println("H      H                I   ")
-  fmt.Println("HHHHHHHH                I   ")
-  fmt.Println("H      H                I   ")
-  fmt.Println("H      H                I   ")
-  fmt.Println("H      H            IIIIIIII") 
+func Chrous(){
+  fmt.Println("Show me how want it to be Tell me, baby, 'cause I need to know now, oh because My loneliness is killing me (and I) I must confess I still        believe(still believe)When I'm not with you I lose my mind Give me a sign Hit me, baby, one more time")
 }
 
 func main() {
-    //call your ascii art function
-    art()
+    fmt.Println("Intro")
+    fmt.Println("Oh baby, baby Oh baby, baby") 
+    fmt.Println("verse1")
+    fmt.Println("Oh baby, baby, how was I supposed to know That something wasn't right   here? Oh baby, baby, I shouldn't have let you go And now you're out of  sight, yeah")   
+    Chrous()
+    fmt.Println("Verse2")
+    fmt.Println("Oh baby, baby The reason I breathe is you Boy, you got me blinded Oh, pretty baby There's nothing that I wouldn't do It's not the way I planned it")
+    Chrous()
+    
 }
