742358fb7aa822c4c894b0ebff712ace4525241f
ofeargul <Chadfarrell@live.com>
2020-04-02 23:56:38 +0000

    3.2.1 Random Feedback

--- main.go
+++ main.go
@@ -1,24 +1,24 @@
-// Programmer name: Chad Farrell
-// Date completed: 3.26.2020
-// Description: this function creates plays britney spears
+// Programmer name: chad farrell
+// Date completed:  4/2/2020
+// Description: trivial game that uses functions to give feedback
 
 package main
 
 import "fmt"
-
-//create a function that draws ASCII art of some type
-func Chrous(){
-  fmt.Println("Show me how want it to be Tell me, baby, 'cause I need to know now, oh because My loneliness is killing me (and I) I must confess I still        believe(still believe)When I'm not with you I lose my mind Give me a sign Hit me, baby, one more time")
+var anwser int
+//Function if correect
+func Pass(){
+  fmt.Println("Correct good job!!")
 }
-
+func Fail(){
+  fmt.Println("Oops wrong!!")
+}
 func main() {
-    fmt.Println("Intro")
-    fmt.Println("Oh baby, baby Oh baby, baby") 
-    fmt.Println("verse1")
-    fmt.Println("Oh baby, baby, how was I supposed to know That something wasn't right   here? Oh baby, baby, I shouldn't have let you go And now you're out of  sight, yeah")   
-    Chrous()
-    fmt.Println("Verse2")
-    fmt.Println("Oh baby, baby The reason I breathe is you Boy, you got me blinded Oh, pretty baby There's nothing that I wouldn't do It's not the way I planned it")
-    Chrous()
-    
+    fmt.Println("Whats 9 + 11 =?")
+    fmt.Scanln(&anwser)
+    if anwser == 21{
+      Pass()
+    }else{
+      Fail()
+    }
 }
